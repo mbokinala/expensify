6170a26f8c632a80376e82571be03ac91c8c5f43
Manav Bokinala <manav.bokinala@gmail.com>
2017-12-27 13:21:43 -0800

    Converted edit page to explicit return

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense page
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            This is from my edit expense page
+        </div>
+    );
+};
 
 export default EditExpensePage;
